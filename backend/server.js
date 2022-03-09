17a74b90f1141681fee9c14445fd92c2afff61c2
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2022-03-09 16:26:34 +1100

    fix deploy error

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,9 @@ if (process.env.NODE_ENV === 'production') {
   //set frontend build folder as static
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-  );
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
 } else {
   app.get('/', (req, res) => {
     res.json({ message: 'welcome' });
